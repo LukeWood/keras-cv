e52f82b40b255b5cd09fac1a4b29723f7a3b3754
Luke Wood <lukewoodcs@gmail.com>
2022-09-10 17:11:14 -0700

    Some final touches to the RetinaNet API

--- keras_cv/models/object_detection/retina_net/__internal__/layers/prediction_head.py
+++ keras_cv/models/object_detection/retina_net/__internal__/layers/prediction_head.py
@@ -30,7 +30,7 @@ class PredictionHead(layers.Layer):
         or the box regression head depending on `output_filters`.
     """
 
-    def __init__(self, output_filters, bias_initializer, num_conv_layers=1, **kwargs):
+    def __init__(self, output_filters, bias_initializer, num_conv_layers=3, **kwargs):
         super().__init__(**kwargs)
         self.output_filters = output_filters
         self.bias_initializer = bias_initializer
@@ -43,7 +43,6 @@ class PredictionHead(layers.Layer):
                 padding="same",
                 kernel_initializer=tf.keras.initializers.Orthogonal(),
                 activation="relu",
-                kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4),
             )
             for _ in range(num_conv_layers)
         ]
@@ -54,7 +53,6 @@ class PredictionHead(layers.Layer):
             padding="same",
             kernel_initializer=tf.keras.initializers.Orthogonal(),
             bias_initializer=self.bias_initializer,
-            kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4),
         )
 
     def call(self, x, training=False):
